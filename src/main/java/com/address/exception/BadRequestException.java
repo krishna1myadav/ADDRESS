ca23f405ca92cd7086b5f3593fa3ffc44679f3db
krishan1myadav <krishna1myadav.@gmail.com>
2026-09-07 23:29:14 +0530

    BadRequestException Class completed

--- src/main/java/com/address/exception/BadRequestException.java
+++ src/main/java/com/address/exception/BadRequestException.java
@@ -1,5 +1,23 @@
 package com.address.exception;
 
-public class BadRequestException extends RuntimeException{
+import org.springframework.http.HttpStatus;
 
+public class BadRequestException extends RuntimeException{
+    private String message;
+    private HttpStatus status;
+
+    public BadRequestException(String message){
+        super(message);
+        this.message = message;
+        this.status = HttpStatus.BAD_REQUEST;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
 }
