4f58516699795de5f9a73309e966b8a026707d83
krishan1myadav <krishna1myadav.@gmail.com>
2026-09-09 22:28:23 +0530

    ResourceNotFoundException Class completed

--- src/main/java/com/address/exception/ResourceNotFoundException.java
+++ src/main/java/com/address/exception/ResourceNotFoundException.java
@@ -4,5 +4,20 @@ import org.springframework.http.HttpStatus;
 
 public class ResourceNotFoundException extends RuntimeException{
 
+    private String message;
+    private HttpStatus status;
 
+    public ResourceNotFoundException(String message){
+        this.message = message;
+        this.status = HttpStatus.NOT_FOUND;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
 }
