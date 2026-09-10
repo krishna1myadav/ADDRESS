a4cbc6d617f996b2ac22fa5e55208e9e1e410c9f
krishan1myadav <krishna1myadav.@gmail.com>
2026-09-10 23:39:16 +0530

    creating atributes of Address class

--- src/main/java/com/address/model/entity/Address.java
+++ src/main/java/com/address/model/entity/Address.java
@@ -1,6 +1,14 @@
 package com.address.model.entity;
 
 
+import jakarta.persistence.Entity;
 
+@Entity
 public class Address {
+
+    private Long id;
+    private Long empId;
+    private String street;
+    private Long pinCode;
+
 }
