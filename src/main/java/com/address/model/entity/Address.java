0e7ea6b6072281d6600ff8ba90968a9a156c6269
krishan1myadav <krishna1myadav.@gmail.com>
2026-09-11 23:20:25 +0530

    created getter setter for all field of Address Class

--- src/main/java/com/address/model/entity/Address.java
+++ src/main/java/com/address/model/entity/Address.java
@@ -19,5 +19,59 @@ public class Address {
     @Enumerated(EnumType.STRING)
     private AddressType addressType;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(Long empId) {
+        this.empId = empId;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public Long getPinCode() {
+        return pinCode;
+    }
+
+    public void setPinCode(Long pinCode) {
+        this.pinCode = pinCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public AddressType getAddressType() {
+        return addressType;
+    }
+
+    public void setAddressType(AddressType addressType) {
+        this.addressType = addressType;
+    }
 }
