4468af127812dc9216889f33321639f3de1ab845
krishan1myadav <krishna1myadav.@gmail.com>
2026-09-12 22:11:14 +0530

    created the fields of AddressDto class

--- src/main/java/com/address/model/dto/AddressDto.java
+++ src/main/java/com/address/model/dto/AddressDto.java
@@ -1,4 +1,11 @@
 package com.address.model.dto;
 
 public class AddressDto {
+
+    private Long id;
+    private Long empId;
+    private String street;
+    private Long pinCode;
+    private String city;
+    private String country;
 }
