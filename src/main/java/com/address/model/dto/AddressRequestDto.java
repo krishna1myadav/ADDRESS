5d0ee6c363064e0ea2ccc42af79b7eb674a46820
krishan1myadav <krishna1myadav.@gmail.com>
2026-09-15 21:29:40 +0530

    genereting getter and setter for all fields of AddressRequestDto class inside dto package

--- src/main/java/com/address/model/dto/AddressRequestDto.java
+++ src/main/java/com/address/model/dto/AddressRequestDto.java
@@ -11,4 +11,51 @@ public class AddressRequestDto {
     private String country;
     private AddressType addressType;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public Long getPinCode() {
+        return pinCode;
+    }
+
+    public void setPinCode(Long pinCode) {
+        this.pinCode = pinCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public AddressType getAddressType() {
+        return addressType;
+    }
+
+    public void setAddressType(AddressType addressType) {
+        this.addressType = addressType;
+    }
 }
