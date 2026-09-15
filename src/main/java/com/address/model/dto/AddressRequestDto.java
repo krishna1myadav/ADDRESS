205946b074b333b9e381f06b1ebe7cf27abb8b1f
krishan1myadav <krishna1myadav.@gmail.com>
2026-09-15 21:28:07 +0530

    declearing fields of AddressRequestDto class inside dto package

--- src/main/java/com/address/model/dto/AddressRequestDto.java
+++ src/main/java/com/address/model/dto/AddressRequestDto.java
@@ -5,6 +5,10 @@ import com.address.model.enums.AddressType;
 public class AddressRequestDto {
 
     private Long id;
-
+    private String street;
+    private Long pinCode;
+    private String city;
+    private String country;
+    private AddressType addressType;
 
 }
