39da2506da0fefa340541fb98ae8b57acf6a93dc
krishan1myadav <krishna1myadav.@gmail.com>
2026-09-16 23:23:03 +0530

    Generating getter and setter for all the fields of AddressRequest inside dto package

--- src/main/java/com/address/model/dto/AddressRequest.java
+++ src/main/java/com/address/model/dto/AddressRequest.java
@@ -6,5 +6,19 @@ public class AddressRequest {
     private Long empId;
     private List<AddressRequestDto> addressRequestDtoList;
 
+    public Long getEmpId() {
+        return empId;
+    }
 
+    public void setEmpId(Long empId) {
+        this.empId = empId;
+    }
+
+    public List<AddressRequestDto> getAddressRequestDtoList() {
+        return addressRequestDtoList;
+    }
+
+    public void setAddressRequestDtoList(List<AddressRequestDto> addressRequestDtoList) {
+        this.addressRequestDtoList = addressRequestDtoList;
+    }
 }
