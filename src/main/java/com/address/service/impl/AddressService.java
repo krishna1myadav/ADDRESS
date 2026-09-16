1b134a00fc919e5243dad2a1fe9027c321736c22
krishan1myadav <krishna1myadav.@gmail.com>
2026-09-16 23:30:17 +0530

    created methods in AddressService interfacee

--- src/main/java/com/address/service/impl/AddressService.java
+++ src/main/java/com/address/service/impl/AddressService.java
@@ -1,8 +1,15 @@
 package com.address.service.impl;
 
 import com.address.model.dto.AddressDto;
+import com.address.model.dto.AddressRequest;
+
+import java.util.List;
 
 public interface AddressService {
 
-    AddressDto saveAddress();
+    AddressDto saveAddress(AddressRequest addressRequest);
+    AddressDto updateAddress(AddressRequest addressRequest);
+    AddressDto getSingleAddress(Long id);
+    List<AddressDto> getAllAddress();
+    void deleteAddress(Long id);
 }
