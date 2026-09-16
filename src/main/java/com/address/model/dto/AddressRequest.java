278cb7ee5a36936a42ac2c8486adf32064603b5d
krishan1myadav <krishna1myadav.@gmail.com>
2026-09-16 23:21:30 +0530

    Decleared the fields of AddressRequest inside dto package

--- src/main/java/com/address/model/dto/AddressRequest.java
+++ src/main/java/com/address/model/dto/AddressRequest.java
@@ -1,4 +1,10 @@
 package com.address.model.dto;
 
+import java.util.List;
+
 public class AddressRequest {
+    private Long empId;
+    private List<AddressRequestDto> addressRequestDtoList;
+
+
 }
