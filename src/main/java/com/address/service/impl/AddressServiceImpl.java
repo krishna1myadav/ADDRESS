5a1f369d7aff0c400f9987277d3d585f0aff73e6
krishan1myadav <krishna1myadav.@gmail.com>
2026-09-17 23:19:29 +0530

    implementing all method present in AddressService interface

--- src/main/java/com/address/service/impl/AddressServiceImpl.java
+++ src/main/java/com/address/service/impl/AddressServiceImpl.java
@@ -1,11 +1,16 @@
 package com.address.service.impl;
 
+import com.address.model.dto.AddressDto;
+import com.address.model.dto.AddressRequest;
 import com.address.repository.AddressRepository;
+import com.address.service.AddressService;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class AddressServiceImpl {
+public class AddressServiceImpl implements AddressService {
 
     private final AddressRepository addressRepository;
     private final ModelMapper modelMapper;
@@ -13,5 +18,30 @@ public class AddressServiceImpl {
     public AddressServiceImpl(AddressRepository addressRepository, ModelMapper modelMapper){
         this.addressRepository = addressRepository;
         this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public AddressDto saveAddress(AddressRequest addressRequest) {
+        return null;
+    }
+
+    @Override
+    public AddressDto updateAddress(AddressRequest addressRequest) {
+        return null;
+    }
+
+    @Override
+    public AddressDto getSingleAddress(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<AddressDto> getAllAddress() {
+        return List.of();
+    }
+
+    @Override
+    public void deleteAddress(Long id) {
+
     }
 }
