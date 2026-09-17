66d23aa8767ec46857b2d4cf73dcf3cdbefb2b6c
krishan1myadav <krishna1myadav.@gmail.com>
2026-09-17 23:17:11 +0530

    creating a constructor of AddressService Class with parameter address

--- src/main/java/com/address/service/impl/AddressServiceImpl.java
+++ src/main/java/com/address/service/impl/AddressServiceImpl.java
@@ -1,14 +1,17 @@
 package com.address.service.impl;
 
 import com.address.repository.AddressRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AddressServiceImpl {
 
     private final AddressRepository addressRepository;
+    private final ModelMapper modelMapper;
 
-    public AddressServiceImpl(AddressRepository addressRepository){
+    public AddressServiceImpl(AddressRepository addressRepository, ModelMapper modelMapper){
         this.addressRepository = addressRepository;
+        this.modelMapper = modelMapper;
     }
 }
