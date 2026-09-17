9d7e6357ec132c75ee5d2c4af632730ded7281bc
krishan1myadav <krishna1myadav.@gmail.com>
2026-09-17 23:14:31 +0530

    creating a constructor of AddressService Class

--- src/main/java/com/address/service/impl/AddressServiceImpl.java
+++ src/main/java/com/address/service/impl/AddressServiceImpl.java
@@ -1,7 +1,14 @@
 package com.address.service.impl;
 
+import com.address.repository.AddressRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AddressServiceImpl {
+
+    private final AddressRepository addressRepository;
+
+    public AddressServiceImpl(AddressRepository addressRepository){
+        this.addressRepository = addressRepository;
+    }
 }
