2d145ba92e4d51562413690ea650afb94e88cc1a
krishan1myadav <krishna1myadav.@gmail.com>
2026-09-20 22:13:53 +0530

    creating saveAddress method

--- src/main/java/com/address/controller/AddressController.java
+++ src/main/java/com/address/controller/AddressController.java
@@ -1,8 +1,14 @@
 package com.address.controller;
 
+import com.address.model.dto.AddressDto;
+import com.address.model.dto.AddressRequest;
 import com.address.service.AddressService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/addresses")
@@ -12,6 +18,10 @@ public class AddressController {
 
     public AddressController(AddressService addressService){
         this.addressService = addressService;
+    }
+
+    public ResponseEntity<List<AddressDto>> saveAddress(@RequestBody AddressRequest addressDto){
+
     }
 
 }
