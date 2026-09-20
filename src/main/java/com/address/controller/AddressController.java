537604f034890886dbb833405f2437cab8a5e807
krishan1myadav <krishna1myadav.@gmail.com>
2026-09-20 22:06:40 +0530

    given RestController and RequestMapping anotation with addresses endpoit

--- src/main/java/com/address/controller/AddressController.java
+++ src/main/java/com/address/controller/AddressController.java
@@ -1,10 +1,14 @@
 package com.address.controller;
 
+import com.address.service.AddressService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/addresses")
 public class AddressController {
+
+    private final AddressService addressService;
+
 
 }
