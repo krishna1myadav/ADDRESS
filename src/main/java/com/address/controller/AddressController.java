132a29f7e634f0c3a526fbbe3583a74a4948ef87
krishan1myadav <krishna1myadav.@gmail.com>
2026-09-20 22:07:48 +0530

    creating AddressController constructor

--- src/main/java/com/address/controller/AddressController.java
+++ src/main/java/com/address/controller/AddressController.java
@@ -10,5 +10,8 @@ public class AddressController {
 
     private final AddressService addressService;
 
+    public AddressController(AddressService addressService){
+        this.addressService = addressService;
+    }
 
 }
